09f3820df959a3741019043cf9d2cb6cee62ce81
xhm1027 <xhm1027@gmail.com>
2013-03-05 11:11:01 +0800

    change db pass

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ module.exports = {
 	db: 'heroku_app12065550',
 	host: 'ds033907.mongolab.com',
 	port: '33907',
-	user: 'xhm1027',
-	password: 'mongolab798155',
-	mongoUri : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://xhm1027:mongolab798155@ds033907.mongolab.com:33907/heroku_app12065550?safe=true',
+	user: 'mongolab.xhm',
+	password: 'mongolabtimeline',
+	mongoUri : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://mongolab.xhm:mongolabtimeline@ds033907.mongolab.com:33907/heroku_app12065550?safe=true',
 };
