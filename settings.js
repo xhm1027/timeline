91e8354c42559e307958c010a8b240c3670fb8cf
xhm1027 <xhm1027@gmail.com>
2013-03-05 11:12:04 +0800

    change db pass

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 module.exports = {
 	cookieSecret: 'timelinebyxhm',
-	db: 'heroku_app12065550',
+	db: 'heroku_app12832052',
 	host: 'ds033907.mongolab.com',
 	port: '33907',
 	user: 'mongolab.xhm',
 	password: 'mongolabtimeline',
-	mongoUri : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://mongolab.xhm:mongolabtimeline@ds033907.mongolab.com:33907/heroku_app12065550?safe=true',
+	mongoUri : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://mongolab.xhm:mongolabtimeline@ds033907.mongolab.com:33907/heroku_app12832052?safe=true',
 };
