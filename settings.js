2b80058f485c717264eb88b70b95d891136db3bf
xhm1027 <xhm1027@gmail.com>
2013-03-05 22:16:21 +0800

    change db port

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ module.exports = {
 	cookieSecret: 'timelinebyxhm',
 	db: 'heroku_app12832052',
 	host: 'ds033907.mongolab.com',
-	port: '33907',
+	port: '33797',
 	user: 'mongolab.xhm',
 	password: 'mongolabtimeline',
-	mongoUri : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://mongolab.xhm:mongolabtimeline@ds033907.mongolab.com:33907/heroku_app12832052?safe=true',
+	mongoUri : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://mongolab.xhm:mongolabtimeline@ds033797.mongolab.com:33797/heroku_app12832052?safe=true',
 };
