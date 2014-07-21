a2690e6c9ed33930706dfe725ba4c5140dbc469c
xhm1027 <xhm1027@gmail.com>
2014-07-21 22:46:21 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ module.exports = {
 	host: 'ds033907.mongolab.com',
 	port: '33797',
 	user: 'mongolab.xhm',
-	password: 'Mongolabtimeline',
+	password: 'mongolab1027',
 	mongoUri : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://mongolab.xhm:mongolabtimeline@ds033797.mongolab.com:33797/heroku_app12832052?safe=true',
 };
